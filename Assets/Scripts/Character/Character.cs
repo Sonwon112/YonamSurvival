a67c60d47d61f88c3b180bb7697826c4cb3cb751
Sonwonill <tlatmsdkqk@gmail.com>
2025-03-25 14:15:03 +0900

    :sparkles: Feat : "배경화면 배치, 문 접근 시 가이드 텍스트 띄우는 작업 완료"

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -1,8 +1,12 @@
+using System;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    [SerializeField] private float speed = 1.0f;
+    [Header("State Variable")]
+    [SerializeField] private float speed = 3.0f;
+    [SerializeField] private float runSpeed = 5.5f;
+    [SerializeField] private float diagonalTmp = 0.7f;
 
     private Rigidbody2D rbCharacter;
     private Animator animator;
@@ -19,6 +23,10 @@ public class Character : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        float isRun = Input.GetAxis("Run");
+        float tmpSpeed = isRun > 0 ? runSpeed : speed;
+
+        animator.SetFloat("RunSpeed",tmpSpeed);
 
         if(horizontal != 0)
         {
@@ -26,10 +34,14 @@ public class Character : MonoBehaviour
             transform.localScale = new Vector3(scaleX, 1, 1);
         }
 
+        float absHor = Mathf.Abs(horizontal);
+        float absVert = Mathf.Abs(vertical);
+
+        float movingSpeed = absHor + absVert > 1.1 ? tmpSpeed * diagonalTmp : tmpSpeed;
+
+        rbCharacter.linearVelocity = new Vector2(horizontal* movingSpeed, vertical* movingSpeed);
         
-        rbCharacter.linearVelocity = new Vector2(horizontal*speed, vertical*speed);
-        
-        animator.SetFloat("speed", Mathf.Abs(rbCharacter.linearVelocityX) + Mathf.Abs(rbCharacter.linearVelocityY));
+        animator.SetFloat("speed", absHor + absVert);
 
     }
 }
