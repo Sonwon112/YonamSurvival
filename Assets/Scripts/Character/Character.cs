4275341040d1444278741b2d10b249d2792cffad
Sonwonill <tlatmsdkqk@gmail.com>
2025-03-30 21:48:46 +0900

    :sparkles: Feat : "구르기 기능 구현"

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -7,9 +7,15 @@ public class Character : MonoBehaviour
     [SerializeField] private float speed = 3.0f;
     [SerializeField] private float runSpeed = 5.5f;
     [SerializeField] private float diagonalTmp = 0.7f;
+    [SerializeField] private float rollPow = 1f;
 
     private Rigidbody2D rbCharacter;
     private Animator animator;
+
+    
+    private float direction = 1;
+    private bool isRoll = false;
+
 
     // 상호작용
     private bool canInteract;
@@ -35,18 +41,29 @@ public class Character : MonoBehaviour
 
         if(horizontal != 0)
         {
-            float scaleX = horizontal < 0 ? -1 : 1;
-            transform.localScale = new Vector3(scaleX, 1, 1);
+            direction= horizontal < 0 ? -1 : 1;
+            transform.localScale = new Vector3(direction, 1, 1);
         }
 
         float absHor = Mathf.Abs(horizontal);
         float absVert = Mathf.Abs(vertical);
 
         float movingSpeed = absHor + absVert > 1.1 ? tmpSpeed * diagonalTmp : tmpSpeed;
+        float vx = horizontal * movingSpeed;
+        float currRollPow = isRoll ? direction * rollPow : 0f;
 
-        rbCharacter.linearVelocity = new Vector2(horizontal* movingSpeed, vertical* movingSpeed);
+        //Debug.Log(isRoll);
+
+        rbCharacter.linearVelocity = new Vector2(horizontal* movingSpeed + currRollPow, vertical* movingSpeed);
         
         animator.SetFloat("speed", absHor + absVert);
+
+        if (Input.GetButtonDown("Jump") && !isRoll)
+        {
+            //isRoll = true;
+            animator.SetBool("roll", true);
+            Invoke("setRollTrue", 0.1f);
+        }
 
         if (canInteract)
         {
@@ -65,6 +82,17 @@ public class Character : MonoBehaviour
     {
         this.canInteract = canInteract;
         this.interactTarget = interactTarget;
+    }
+
+    public void setRollTrue()
+    {
+        isRoll = true;
+    }
+
+    public void setRollFalse()
+    {
+        isRoll = false;
+        animator.SetBool("roll", false);
     }
 
 }
